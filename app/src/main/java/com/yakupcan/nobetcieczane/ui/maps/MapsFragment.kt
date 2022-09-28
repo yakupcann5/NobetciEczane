566b6955e7617ac0f3a292e2212219276fc06574
Murat Aktay <murat.aktay@modanisa.com>
2022-09-28 17:50:37 +0300

    lastlocation problem fixed

--- app/src/main/java/com/yakupcan/nobetcieczane/ui/maps/MapsFragment.kt
+++ app/src/main/java/com/yakupcan/nobetcieczane/ui/maps/MapsFragment.kt
@@ -13,8 +13,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
-import com.yakupcan.nobetcieczane.R
-import com.yakupcan.nobetcieczane.databinding.FragmentMapWithBottomsheetBinding
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -22,30 +20,29 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
-import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.play.core.review.ReviewManagerFactory
-import com.google.android.play.core.review.model.ReviewErrorCode
+import com.yakupcan.nobetcieczane.R
+import com.yakupcan.nobetcieczane.databinding.FragmentMapWithBottomsheetBinding
 import dagger.hilt.android.AndroidEntryPoint
-import java.util.*
 
 
 @AndroidEntryPoint
 class MapsFragment : Fragment(), View.OnClickListener {
-    private lateinit var binding: FragmentMapWithBottomsheetBinding
-    private val viewModel: MapsFragmentViewModel by viewModels()
+    private lateinit var binding : FragmentMapWithBottomsheetBinding
+    private val viewModel : MapsFragmentViewModel by viewModels()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+        inflater : LayoutInflater, container : ViewGroup?,
+        savedInstanceState : Bundle?
+    ) : View {
         binding = FragmentMapWithBottomsheetBinding.inflate(layoutInflater)
         MobileAds.initialize(this.requireContext()) {}
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view : View, savedInstanceState : Bundle?) {
         initViews()
         mapReady()
         setBottomSettingSheet(false)
@@ -56,28 +53,34 @@ class MapsFragment : Fragment(), View.OnClickListener {
         inAppReviews()
     }
 
-    override fun onClick(v: View?) {
+    override fun onClick(v : View?) {
         when (v?.id) {
             R.id.filter_fragment_open_bttn -> {
                 findNavController().navigate(R.id.action_mapsFragment2_to_listFragment2)
             }
+
             R.id.settings_button -> {
                 setBottomSettingSheet(true)
             }
+
             R.id.setting_sheet_settings_text -> {
                 findNavController().navigate(R.id.action_mapsFragment2_to_settingsFragment)
             }
+
             R.id.setting_sheet_vote_text -> {
                 Toast.makeText(requireContext(), "oylama", Toast.LENGTH_SHORT).show()
             }
+
             R.id.setting_sheet_share_text -> {
                 Toast.makeText(requireContext(), "paylaş", Toast.LENGTH_SHORT).show()
             }
+
             R.id.pharmacyCall -> {
                 val intent = Intent(Intent.ACTION_DIAL);
                 intent.data = Uri.parse("tel:${viewModel.getPhoneNum()}")
                 startActivity(intent)
             }
+
             R.id.pharmacyDirections -> {
                 val gmmIntentUri =
                     Uri.parse("google.navigation:q=${viewModel.getMarkerLat()}, ${viewModel.getMarkerLng()}")
@@ -105,7 +108,12 @@ class MapsFragment : Fragment(), View.OnClickListener {
         bottomSheetBehavior.peekHeight = binding.bottomsheet.constraint.height
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync { map ->
-            map.setMapStyle(MapStyleOptions.loadRawResourceStyle(this.requireContext(),R.raw.style_json))
+            map.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(
+                    this.requireContext(),
+                    R.raw.style_json
+                )
+            )
             val pharmacyList = arrayListOf<LatLng>()
             val nowLocation = arrayListOf<LatLng>()
             viewModel.markerList.observe(viewLifecycleOwner, Observer { list ->
@@ -145,9 +153,11 @@ class MapsFragment : Fragment(), View.OnClickListener {
                             .setPositiveButton(R.string.cancel,
                                 DialogInterface.OnClickListener { dialogInterface, i ->
                                     dialogInterface.cancel()
-                                }).setNegativeButton(R.string.try_again, DialogInterface.OnClickListener{ dialogInterface, i ->
+                                }).setNegativeButton(
+                                R.string.try_again,
+                                DialogInterface.OnClickListener { dialogInterface, i ->
                                     findNavController().navigate(R.id.action_mapsFragment2_to_splashFragment)
-                            })
+                                })
                     alertDialog.show()
                 }
             })
@@ -182,18 +192,20 @@ class MapsFragment : Fragment(), View.OnClickListener {
 
                             bottomSheetBehavior.setBottomSheetCallback(object :
                                 BottomSheetBehavior.BottomSheetCallback() {
-                                override fun onStateChanged(bottomSheet: View, newState: Int) {
+                                override fun onStateChanged(bottomSheet : View, newState : Int) {
                                     when (newState) {
                                         BottomSheetBehavior.STATE_DRAGGING, BottomSheetBehavior.STATE_SETTLING -> {}
+
                                         BottomSheetBehavior.STATE_HIDDEN -> {
                                             bottomSheetBehavior.peekHeight = 0
                                             binding.bottomsheet.constraint.visibility = View.GONE
                                         }
+
                                         else -> {}
                                     }
                                 }
 
-                                override fun onSlide(bottomSheet: View, slideOffset: Float) {}
+                                override fun onSlide(bottomSheet : View, slideOffset : Float) {}
                             })
                         }
 
@@ -210,7 +222,7 @@ class MapsFragment : Fragment(), View.OnClickListener {
         }
     }
 
-    private fun setBottomSettingSheet(isVisible: Boolean) {
+    private fun setBottomSettingSheet(isVisible : Boolean) {
         val bottomSheetSetting = BottomSheetBehavior.from(binding.settingsSheet.root)
         bottomSheetSetting.state = BottomSheetBehavior.STATE_HIDDEN
         bottomSheetSetting.peekHeight = binding.settingsSheet.constraint.height
@@ -219,15 +231,17 @@ class MapsFragment : Fragment(), View.OnClickListener {
             bottomSheetSetting.state = BottomSheetBehavior.STATE_EXPANDED
             bottomSheetSetting.setBottomSheetCallback(object :
                 BottomSheetBehavior.BottomSheetCallback() {
-                override fun onStateChanged(bottomSheet: View, newState: Int) {
+                override fun onStateChanged(bottomSheet : View, newState : Int) {
                     when (newState) {
                         BottomSheetBehavior.STATE_DRAGGING, BottomSheetBehavior.STATE_SETTLING -> {}
+
                         BottomSheetBehavior.STATE_HIDDEN -> {}
+
                         else -> {}
                     }
                 }
 
-                override fun onSlide(bottomSheet: View, slideOffset: Float) {}
+                override fun onSlide(bottomSheet : View, slideOffset : Float) {}
             })
         } else {
             binding.settingsSheet.constraint.visibility = View.GONE
@@ -241,7 +255,7 @@ class MapsFragment : Fragment(), View.OnClickListener {
             if (task.isSuccessful) {
                 // We got the ReviewInfo object
                 val reviewInfo = task.result
-                val flow = manager.launchReviewFlow(this.requireActivity(),reviewInfo)
+                val flow = manager.launchReviewFlow(this.requireActivity(), reviewInfo)
                 flow.addOnCompleteListener {
 
                 }
--- app/src/main/java/com/yakupcan/nobetcieczane/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/yakupcan/nobetcieczane/ui/splash/SplashFragment.kt
@@ -1,8 +1,7 @@
 package com.yakupcan.nobetcieczane.ui.splash
 
 import android.Manifest
-import android.content.DialogInterface
-import android.content.Intent
+import android.annotation.SuppressLint
 import android.content.IntentSender
 import android.content.pm.PackageManager
 import android.location.Geocoder
@@ -13,41 +12,42 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import com.yakupcan.nobetcieczane.R
-import com.yakupcan.nobetcieczane.databinding.FragmentSplashBinding
 import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.LocationSettingsRequest
 import com.google.android.gms.tasks.Task
+import com.yakupcan.nobetcieczane.R
+import com.yakupcan.nobetcieczane.databinding.FragmentSplashBinding
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.*
 
+
 @AndroidEntryPoint
 class SplashFragment : Fragment(), LocationListener {
-    private lateinit var binding: FragmentSplashBinding
-    private val splashViewModel: SplashViewModel by viewModels()
+    private lateinit var binding : FragmentSplashBinding
+    private val splashViewModel : SplashViewModel by viewModels()
     private var permissionControl = 0
-    private lateinit var flpc: FusedLocationProviderClient
-    private lateinit var locationTask: Task<Location>
+    private lateinit var flpc : FusedLocationProviderClient
+    private lateinit var locationTask : Task<Location>
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+        inflater : LayoutInflater, container : ViewGroup?,
+        savedInstanceState : Bundle?
+    ) : View {
         binding = FragmentSplashBinding.inflate(inflater)
         flpc = LocationServices.getFusedLocationProviderClient(this.requireContext())
         Handler(Looper.getMainLooper()).postDelayed({
@@ -57,8 +57,13 @@ class SplashFragment : Fragment(), LocationListener {
     }
 
     @RequiresApi(Build.VERSION_CODES.N)
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view : View, savedInstanceState : Bundle?) {
         //onLocationChanged(splashViewModel.getLat(),splashViewModel.getLng())
+        checkPermissions()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun checkPermissions() {
         val locationPermissionRequest = registerForActivityResult(
             ActivityResultContracts.RequestMultiplePermissions()
         ) { permissions ->
@@ -66,40 +71,46 @@ class SplashFragment : Fragment(), LocationListener {
                 permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
                     // Precise location access granted.
                     checked()
-
-                    Log.d("yakup", "location: 3")
-/*
-                    checked()
-*/
                 }
+
                 permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
                     // Only approximate location access granted.
-/*
-                    location()
-*/
+                    checked()
                 }
+
                 else -> {
                     // No location access granted.
                     findNavController().navigate(R.id.action_splashFragment_to_filterFragment)
                 }
             }
         }
-        locationPermissionRequest.launch(
-            arrayOf(
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION
+
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            locationPermissionRequest.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
             )
-        )
+        } else {
+            checked()
+        }
     }
 
+    @SuppressLint("MissingPermission")
     private fun getLocationInformation() {
-        locationTask.addOnSuccessListener {
+        flpc.lastLocation.addOnSuccessListener(requireActivity()) {
             if (it != null) {
                 onLocationChanged(it)
                 //checked()
                 splashViewModel.saveLocationLatLng(it.latitude, it.longitude)
+            } else {
             }
-        }.addOnFailureListener { it -> Log.d("Location Error", it.localizedMessage) }
+        }.addOnFailureListener { it -> Log.d("Location Error", it.localizedMessage.toString()) }
     }
 
 //    @SuppressLint("MissingSuperCall")
@@ -145,72 +156,116 @@ class SplashFragment : Fragment(), LocationListener {
         }*/
     }
 
-    override fun onLocationChanged(location: Location) {
+    override fun onLocationChanged(location : Location) {
+        Log.d("Onlocationchanged: ", "Girdi")
         val geocoder = Geocoder(this.requireContext(), Locale.getDefault())
         val locationList = geocoder.getFromLocation(location.latitude, location.longitude, 1)
         locationList.forEach {
+            it?.toString()?.let { it1 -> Log.d("Address: ", it1) }
             if (it == null) {
                 val alertDialog =
                     AlertDialog.Builder(this.requireContext()).setTitle(R.string.settings)
                         .setMessage(R.string.new_update).setCancelable(true)
-                        .setPositiveButton(R.string.cancel,
-                            DialogInterface.OnClickListener { dialogInterface, i ->
-                                dialogInterface.cancel()
-                            })
+                        .setPositiveButton(
+                            R.string.cancel
+                        ) { dialogInterface, _ ->
+                            dialogInterface.cancel()
+                        }
                 alertDialog.show()
             } else {
+                Log.d("Latitude: ", it.latitude.toString())
+                Log.d("Longitude: ", it.longitude.toString())
+                splashViewModel.saveLocationLatLng(it.latitude, it.longitude)
                 splashViewModel.saveCityTown(it.adminArea, it.subAdminArea)
             }
         }
     }
 
     private fun checked() {
-        val location = LocationRequest.create().setInterval(10000).setFastestInterval(5000)
-            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+        val location = LocationRequest.create().setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+            .setInterval(60000).setFastestInterval(5000)
         val builder = LocationSettingsRequest.Builder().addLocationRequest(location)
         val client = LocationServices.getSettingsClient(this.requireContext())
         val task = client.checkLocationSettings(builder.build())
 
+
         task.addOnSuccessListener {
-            if (ActivityCompat.checkSelfPermission(
-                    this.requireContext(),
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                    this.requireContext(),
-                    Manifest.permission.ACCESS_COARSE_LOCATION
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                findNavController().navigate(R.id.action_splashFragment_to_filterFragment)
-            } else {
-                locationTask = flpc.lastLocation
-                getLocationInformation()
-            }
+            checkPermissionAndOpenMaps()
         }
         task.addOnFailureListener {
             if (it is ResolvableApiException) {
                 try {
-                    val resolvable = it as ResolvableApiException
-                    resolvable.startResolutionForResult(this.requireActivity(), 2)
-                } catch (sendEx: IntentSender.SendIntentException) {
+                    val intentSenderRequest : IntentSenderRequest =
+                        IntentSenderRequest.Builder(it.resolution.intentSender)
+                            .build()
+                    locationRequestHandler.launch(intentSenderRequest)
+                } catch (sendEx : IntentSender.SendIntentException) {
                     // Ignore the error.
                 }
             }
         }
     }
-/*
-    @Deprecated(
-        "Deprecated in Java", ReplaceWith(
-            "super.onActivityResult(requestCode, resultCode, data)",
-            "androidx.fragment.app.Fragment"
-        )
-    )
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (resultCode == AppCompatActivity.RESULT_OK && requestCode == 2) {
-            Log.d("result ok", data.toString())
-            location()
-        } else if (resultCode == AppCompatActivity.RESULT_CANCELED) {
-            Log.d("result cancelled", data.toString())
+
+    @SuppressLint("MissingPermission")
+    private val locationRequestHandler = registerForActivityResult(
+        ActivityResultContracts.StartIntentSenderForResult()
+    ) {
+        flpc.getCurrentLocation(100, null)
+            .addOnSuccessListener {
+                if (it != null)
+                    onLocationChanged(it)
+                else
+                    Log.d("Olmadı be usta. ", "Çalışmadı")
+            }
+    }
+
+    private fun checkPermissionAndOpenMaps() {
+        if (ActivityCompat.checkSelfPermission(
+                this.requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this.requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.d("Location: ", "Location open failed!")
+            findNavController().navigate(R.id.action_splashFragment_to_filterFragment)
+        } else {
+            Log.d("Location: ", "Location opened!")
+            locationTask = flpc.lastLocation
+            getLocationInformation()
         }
-        super.onActivityResult(requestCode, resultCode, data)
-    }*/
+    }
+
+    /*
+        @Deprecated(
+            "Deprecated in Java", ReplaceWith(
+                "super.onActivityResult(requestCode, resultCode, data)",
+                "androidx.fragment.app.Fragment"
+            )
+        )
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            if (resultCode == AppCompatActivity.RESULT_OK && requestCode == 2) {
+                Log.d("result ok", data.toString())
+                location()
+            } else if (resultCode == AppCompatActivity.RESULT_CANCELED) {
+                Log.d("result cancelled", data.toString())
+            }
+            super.onActivityResult(requestCode, resultCode, data)
+        }*/
+/*    fun openSomeActivityForResult() {
+        val intent = Intent(this.requireContext(), MainActivity::class.java)
+        someActivityResultLauncher.launch(intent)
+    }
+
+    // You can do the assignment inside onAttach or onCreate, i.e, before the activity is displayed
+    private var someActivityResultLauncher : ActivityResultLauncher<Intent> =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                // There are no request codes
+                val data : Intent? = result.data
+            }
+        }*/
 }
